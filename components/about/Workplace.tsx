d16a9c24202148ac31bcd800bdba629bf1a330a4
Richard van Brunschot <richardvanbrunschot@gmail.com>
2024-12-29 10:57:23 +0100

    optimize workplace component

--- components/about/Workplace.tsx
+++ components/about/Workplace.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-
 import type { WorkplaceProps } from '@/types';
 
 const Workplace = ({
@@ -10,8 +9,8 @@ const Workplace = ({
   location,
   description,
 }: WorkplaceProps) => {
-  const content = (
-    <>
+  return (
+    <li className="transition-opacity" key={company}>
       <div className="flex gap-4">
         <Image
           src={imageSrc}
@@ -20,21 +19,18 @@ const Workplace = ({
           height={48}
           className="mb-auto rounded-full"
         />
-        <div>
+        <div className="flex-1">
           <p className="font-medium">{title}</p>
           <p className="text-secondary">{company}</p>
         </div>
-        <div className="ml-auto shrink-0">
+        <div className="ml-auto shrink-0 text-right">
           <p className="text-sm text-secondary">{time}</p>
-          <p className="mt-0.5 text-right text-sm text-secondary">{location}</p>
+          <p className="mt-0.5 text-sm text-secondary">{location}</p>
         </div>
       </div>
-      <p className="mt-2 text-sm text-secondary">{description}</p>
-    </>
-  );
-  return (
-    <li className="transition-opacity" key={company}>
-      <div className="w-full gap-1 no-underline">{content}</div>
+      {description && (
+        <p className="mt-2 text-sm text-secondary">{description}</p>
+      )}
     </li>
   );
 };
